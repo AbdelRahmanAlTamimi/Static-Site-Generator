9d4bda57e27cef4e95fabefb97eb46563626f89a
Abdelrahman Altamimi <tamimi6030@gmail.com>
2025-01-24 20:13:04 +0300

    add split nodes delimiter function and test it

---
+++ src/utils.py
@@ -0,0 +1,46 @@
+from textnode import TextType,TextNode
+from htmlnode import LeafNode
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type) :
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        parts = node.text.split(delimiter)
+        if len(parts) % 2 == 0:
+            raise ValueError(
+                f"Invalid syntax: unmatched delimiter '{delimiter}' in text: {node.text}"
+            )
+        for i, part in enumerate(parts):
+            if part == "":
+                continue
+            if i % 2 == 0:
+                new_nodes.append(TextNode(part, "text"))
+            else:
+                new_nodes.append(TextNode(part, text_type))
+
+    return new_nodes
+
+
+
+
+
+            
+            
+        
